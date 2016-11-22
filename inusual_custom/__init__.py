f0a1c6a630d7ce1e6a546f1972e4668a3cc2ddd3
Santi Argüeso <santi@comunitea.com>
2016-11-22 19:17:47 +0100

    [ADD]. inusual_custom/. Modulo para pequeñas modificaciones. Este commit tiene el cambio de posición del campo referencia del cliente

---
+++ inusual_custom/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# © 2016 Comunitea - Santi Argüeso <santi@comunitea.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
---
+++ inusual_custom/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2016 Comunitea - Santi Argüeso <santi@comunitea.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'Inusual Custom ',
+    'version': '8.0.0.0.0',
+    'author': 'Comunitea ',
+    "category": "Custom",
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+    ],
+    'contributors': [
+        "Comunitea ",
+        "Santi Argüeso <santi@comunitea.com>"
+    ],
+    "data": [
+        "views/res_partner_view.xml",
+    ],
+    "demo": [
+    ],
+    'test': [
+    ],
+    "installable": True
+}
