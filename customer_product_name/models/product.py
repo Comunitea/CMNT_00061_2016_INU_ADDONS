20a88a8f5e2eb3ded92a9a8909dcad2d4964136d
Omar (Comunitea) <omar@comunitea.com>
2020-03-31 19:50:12 +0200

    [FIX] customer_product_name: Descripción de modelo

--- customer_product_name/models/product.py
+++ customer_product_name/models/product.py
@@ -7,6 +7,7 @@ from odoo import models, fields
 class ProductCustomer(models.Model):
 
     _name = 'product.customer'
+    _description = "Customer's product names"
 
     name = fields.Char('Name', size=64)
     code = fields.Char('Code', size=64)
